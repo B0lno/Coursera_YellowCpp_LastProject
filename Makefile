67f185a9f9f99bf3236aed7213d63ec92ff2ce2c
Yuri Ken <yuri.ken.daz@gmail.com>
2021-04-16 16:13:36 +0300

    Add main.cpp, white cpp solve, fix makefile

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ date: date.cpp date.h
 
 database: date.h date.cpp database.cpp database.h
 	g++ database.cpp -c -o database.o
+
+main: date.o database.o date.h database.h
+	g++ main.cpp -o main database.o date.o
---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include "database.h"
+#include "date.h"
+
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+using namespace std;
+
+int main() {
+  try {
+    Database db;
+
+    string command_line;
+    while (getline(cin, command_line)) {
+      stringstream ss(command_line);
+
+      string command;
+      ss >> command;
+
+      if (command == "Add") {
+
+        string date_str, event;
+        ss >> date_str >> event;
+        const Date date = ParseDate(date_str);
+        db.AddEvent(date, event);
+
+      } else if (command == "Del") {
+
+        string date_str;
+        ss >> date_str;
+        string event;
+        if (!ss.eof()) {
+          ss >> event;
+        }
+        const Date date = ParseDate(date_str);
+        if (event.empty()) {
+          const int count = db.DeleteDate(date);
+          cout << "Deleted " << count << " events" << endl;
+        } else {
+          if (db.DeleteEvent(date, event)) {
+            cout << "Deleted successfully" << endl;
+          } else {
+            cout << "Event not found" << endl;
+          }
+        }
+
+      } else if (command == "Find") {
+
+        string date_str;
+        ss >> date_str;
+        const Date date = ParseDate(date_str);
+        for (const string& event : db.Find(date)) {
+          cout << event << endl;
+        }
+
+      } else if (command == "Print") {
+
+        db.Print();
+
+      } else if (!command.empty()) {
+
+        throw logic_error("Unknown command: " + command);
+
+      }
+    }
+  } catch (const exception& e) {
+    cout << e.what() << endl;
+  }
+
+	return 0;
+}
